f192f32d0f3d6b2119c964e07c3110f951195cf6
naveen.singh <naveen.singh@itelasoft.com>
2024-03-27 09:51:12 +0530

    removed demo data

--- server/index.js
+++ server/index.js
@@ -14,9 +14,6 @@ import { createPost } from "./controllers/posts.js";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
 import { verifyToken } from "./middleware/auth.js";
-import User from "./models/User.js";
-import Post from "./models/Post.js";
-import { users, posts } from "./data/index.js";
 
 /* Configurations */
 
@@ -64,9 +61,5 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-
-    /* Add Data one time */
-    // User.insertMany(users);
-    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
